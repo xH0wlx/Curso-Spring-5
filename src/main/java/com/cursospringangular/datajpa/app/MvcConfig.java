36a6c18041d48f9bff3bbf41b544f0aabafff153
xH0wlx <baton_hp@hotmail.com>
2018-02-10 22:09:10 -0300

    Sección 5: Carpeta de uploads dentro del proyecto, pero desacoplada)

--- src/main/java/com/cursospringangular/datajpa/app/MvcConfig.java
+++ src/main/java/com/cursospringangular/datajpa/app/MvcConfig.java
@@ -1,5 +1,9 @@
 package com.cursospringangular.datajpa.app;
 
+import java.nio.file.Paths;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -7,11 +11,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter{
 
+	private final Logger log = LoggerFactory.getLogger(getClass());
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// TODO Auto-generated method stub
 		super.addResourceHandlers(registry);
-		registry.addResourceHandler("/uploads/**").addResourceLocations("file:C:/spring5/resources/static/uploads/");
+		
+		String resourcePath = Paths.get("uploads").toAbsolutePath().toUri().toString();
+		
+		//registry.addResourceHandler("/uploads/**").addResourceLocations("file:C:/spring5/resources/static/uploads/");
+		registry.addResourceHandler("/uploads/**").addResourceLocations(resourcePath);
+		
+		log.info("ResourcePath: " + resourcePath);
+		
 	}
 
 }
--- src/main/java/com/cursospringangular/datajpa/app/controllers/ClienteController.java
+++ src/main/java/com/cursospringangular/datajpa/app/controllers/ClienteController.java
@@ -5,9 +5,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -42,6 +45,8 @@ public class ClienteController {
 	
 	@Autowired
 	private IClienteService clienteService;
+	
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
 	@GetMapping(value="/ver/{id}")
 	public String ver(
@@ -123,15 +128,24 @@ public class ClienteController {
 		if( !foto.isEmpty() ) {
 			//Path directorioRecursos = Paths.get("src//main//resources//static//uploads");
 			//String rootPath = directorioRecursos.toFile().getAbsolutePath();
-			String rootPath = "c://spring5//resources//static//uploads";
+			//String rootPath = "c://spring5//resources//static//uploads";
+			String uniqueFilename = UUID.randomUUID().toString() + "_" + foto.getOriginalFilename();
+			
+			Path rootPath = Paths.get("uploads").resolve( uniqueFilename );
+			Path rootAbsolutePath = rootPath.toAbsolutePath();
+			
+			log.info("rootPath: " + rootPath);
+			log.info("rootAbsolutePath: " + rootAbsolutePath);
 			
 			try {
-				byte[] bytes = foto.getBytes();
-				Path rutaCompleta = Paths.get(rootPath + "//" + foto.getOriginalFilename());
-				Files.write(rutaCompleta, bytes);
-				flash.addFlashAttribute("info", "Ha subido correctamente '" + foto.getOriginalFilename() + "'");
+				//byte[] bytes = foto.getBytes();
+				//Path rutaCompleta = Paths.get(rootPath + "//" + foto.getOriginalFilename());
+				//Files.write(rutaCompleta, bytes);
+				Files.copy( foto.getInputStream(), rootAbsolutePath );
 				
-				cliente.setFoto(foto.getOriginalFilename());
+				flash.addFlashAttribute("info", "Ha subido correctamente '" + uniqueFilename + "'");
+				
+				cliente.setFoto( uniqueFilename );
 				
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
