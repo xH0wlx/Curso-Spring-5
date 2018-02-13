2d4571fdc3756acef8ceb868c6179dd724602714
xH0wlx <baton_hp@hotmail.com>
2018-02-13 16:36:47 -0300

    Sección 6 - video 58: Creación de la clase ItemFactura y Asociación OneToMay unidireccional.

--- src/main/java/com/cursospringangular/datajpa/app/models/entity/Factura.java
+++ src/main/java/com/cursospringangular/datajpa/app/models/entity/Factura.java
@@ -1,15 +1,20 @@
 package com.cursospringangular.datajpa.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -18,7 +23,7 @@ import javax.persistence.TemporalType;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
-@Table(name="facturas")
+@Table(name = "facturas")
 public class Factura implements Serializable {
 
 	/**
@@ -27,27 +32,37 @@ public class Factura implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String descripcion;
 	private String observacion;
-	
+
 	@Temporal(TemporalType.DATE)
-	@DateTimeFormat(pattern="yyyy-MM-dd")
-	@Column(name="created_at")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Column(name = "created_at")
 	private Date createdAt;
-	
+
 	// Muchas Facturas (clase en la que estamos) un sólo cliente
 	// Eager => Ej: Por cada Factura va a traer a su cliente, trae todos los datos.
-	// Lazy => A medida que se van invocando los métodos trae los datos (no carga tanto la BD).
-	@ManyToOne(fetch=FetchType.LAZY)
+	// Lazy => A medida que se van invocando los métodos trae los datos (no carga
+	// tanto la BD).
+	@ManyToOne(fetch = FetchType.LAZY)
 	private Cliente cliente;
+
+	// El JoinColumn se agrega por no ser una clase en ambos sentidos.
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "factura_id")
+	private List<ItemFactura> items;
+
+	public Factura() {
+		this.items = new ArrayList<ItemFactura>();
+	}
 
 	@PrePersist
 	public void prePersist() {
-		createdAt = new Date(); 
+		createdAt = new Date();
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
@@ -88,4 +103,15 @@ public class Factura implements Serializable {
 		this.cliente = cliente;
 	}
 
+	public List<ItemFactura> getItems() {
+		return items;
+	}
+
+	public void setItems(List<ItemFactura> items) {
+		this.items = items;
+	}
+
+	public void addItemFactura(ItemFactura item) {
+		this.items.add(item);
+	}
 }
---
+++ src/main/java/com/cursospringangular/datajpa/app/models/entity/ItemFactura.java
@@ -0,0 +1,42 @@
+package com.cursospringangular.datajpa.app.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "factura_items")
+public class ItemFactura implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private Integer cantidad;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public Long calcularImporte() {
+		return cantidad.longValue();
+	}
+}
